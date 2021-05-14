911bd3763b1bed92ba580ecf678a9f35e78bdee0
Bruno Alberto Rongetti Jaime <b.a.r.j.1595@gmail.com>
2021-05-14 03:21:43 -0300

    Ejercicio 1 terminado

---
+++ Ejercicio01/Ejercicio01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ingreso;
+
+            Console.WriteLine("Ingrese número:");
+            ingreso = Convert.ToInt32(Console.ReadLine());
+
+            if (ingreso >= 0)
+            {
+                Console.WriteLine("\nNúmero invertido:");
+                for (int i = 1; i <= ingreso.ToString().Length; i++)
+                {
+                    Console.Write(ingreso.ToString()[ingreso.ToString().Length - i]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nEl numero ingresado es menor o igual a 0");
+            }
+            Console.ReadLine();
+        }
+    }
+}
